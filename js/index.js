adf9d452371911d775f636d3d7b7ae3ad122047b
Pfumiko <50150564+OlegSolomatin@users.noreply.github.com>
2022-12-14 16:46:17 +0300

    add adaptive <767.7px

--- js/index.js
+++ js/index.js
@@ -3,6 +3,14 @@ $(document).ready(function(){
         loop:true,
         margin:34,
         nav:true,
+        responsive: {
+            0: {
+                item: 1,
+            },
+            767.7 :{
+                item: 2,
+            }
+        },
         items:2,
         autoWidth: true,
         lazyLoad:true,
